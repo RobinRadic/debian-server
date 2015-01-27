14768427449b562fd41f4e90e6689cea7627e601
robinradic <robin@radic.nl>
2015-01-27 14:11:32 +0100

    initial commit

---
+++ bin/debian-server.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+var radic = require('../lib'),
+    cli = radic('cli');
+
+require('../lib/commands')(cli);
+cli.title('  |  ' + cli.green.bold('radic') + '  | ' + cli.yellow(radic.app.version) + ' |');
+
+cli.usage('radic [command] ' + cli.gray('[[subcommands]]'));
+cli.parse(process.argv);
+
