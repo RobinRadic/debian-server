98d1783fd10c7144a420e3ca188adb3a80d548bb
robinradic <robin@radic.nl>
2015-01-27 15:16:44 +0100

    Added doc generation tasks and test dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,42 @@ module.exports = function(grunt){
     require('time-grunt')(grunt);
 
     grunt.initConfig({
+        radic_jsdoc: {
+            docs: {
+                options: {
+                    docsPath: 'docs'
+                }
+            }
+        },
+        radic_coverage: {
+            docs: {
+                options: {
+                    exclude: ['lib/cli/_celeri/**'],
+                    frontMatterPath: 'docs/processed-front-matter.yml'
+                }
+            }
+        },
+        radic_jsdoc_mdpages: {
+            docs: {
+                files: [{
+                            src: 'README.md',
+                            dest: 'docs/index.md'
+                        }]
+            }
+        },
+        git: {
+            docs: {
+                options: {
+                    cwd: 'docs',
+                    ignoreErrors: true
+                },
+                commands: [
+                    ['add', { A: true }],
+                    ['commit', { m: 'Auto commit & push' }],
+                    ['push', { u: 'origin' }, 'gh-pages']
+                ]
+            }
+        }
 
     });
 
