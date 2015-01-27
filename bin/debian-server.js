a0c6ecc40db75015fe1f2b920362796e5fb209f4
robinradic <robin@radic.nl>
2015-01-27 14:49:24 +0100

    added buidl

--- bin/debian-server.js
+++ bin/debian-server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-var radic = require('../lib'),
-    cli = radic('cli');
+var radic = require('radic'),
+    cli = radic.cli;
 
 require('../lib/commands')(cli);
 cli.title('  |  ' + cli.green.bold('radic') + '  | ' + cli.yellow(radic.app.version) + ' |');
